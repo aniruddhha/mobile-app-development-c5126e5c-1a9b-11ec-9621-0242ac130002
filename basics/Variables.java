13a67945d7a3bacb400a0de2854f7fbed5c7cdf9
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-21 11:37:21 +0530

    basics : variables and datatypes

---
+++ basics/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 20; // variable
+        age = 30;
+        boolean isOs = true;
+        double price = 56.5;
+        char ch = 's';
+
+        System.out.println("Age is " + age);
+        System.out.println("Is OS " + isOs);
+        System.out.println("Price " + price);
+        System.out.println("Char is " + ch);
+    }
+}

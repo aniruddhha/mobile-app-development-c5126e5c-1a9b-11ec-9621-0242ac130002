9f514cbad81021bc55b5efe740c1b61a93eed2d5
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-21 14:39:52 +0530

    classes and objects

---
+++ clsobj/Main.java
@@ -0,0 +1,16 @@
+package clsobj;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i = 10; // variable
+
+        new Router(); // --> object
+
+        Router rt = new Router(); // rt -> ORV
+
+        System.out.println("Learnt the Classes and Objects");
+
+    }
+}
---
+++ clsobj/Router.java
@@ -0,0 +1,16 @@
+package clsobj;
+
+public class Router {
+
+    int ports;
+    boolean isOn;
+    char led;
+
+    void turnOnOff() {
+
+    }
+
+    void ledBlinking() {
+
+    }
+}

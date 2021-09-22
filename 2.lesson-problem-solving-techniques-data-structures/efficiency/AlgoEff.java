d98b7fc4ed52b0c1ae1f82ed07be6ac8a31b1371
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-22 14:16:37 +0530

    algorithm : efficiency

---
+++ 2.lesson-problem-solving-techniques-data-structures/efficiency/AlgoEff.java
@@ -0,0 +1,14 @@
+package efficiency;
+
+public class AlgoEff {
+
+    public void constantTime() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Checking Constant Time"); // n = 100
+        } // o(n) = o(100) = o(1)
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}

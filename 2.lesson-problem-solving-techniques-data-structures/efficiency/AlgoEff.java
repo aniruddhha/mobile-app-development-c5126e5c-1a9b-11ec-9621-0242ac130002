762528fbb6ec180ec48c3e8fee5869082d5989f2
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-22 14:30:00 +0530

    efficiency : logarithmic

--- 2.lesson-problem-solving-techniques-data-structures/efficiency/AlgoEff.java
+++ 2.lesson-problem-solving-techniques-data-structures/efficiency/AlgoEff.java
@@ -2,13 +2,13 @@ package efficiency;
 
 public class AlgoEff {
 
-    public void constantTime() {
+    public static void constantTime() {
         for (int i = 0; i < 100; i++) {
             System.out.println("Checking Constant Time"); // n = 100
         } // o(n) = o(100) = o(1)
     }
 
-    public void logarithmicTime(int n) { // n = 100
+    public static void logarithmicTime(int n) { // n = 100
         for (int i = 1; i < n; i *= 2) { // i = 1 * 2 = 2
                                          // i = 2 * 2 = 4
                                          // i = 8 * 2
@@ -17,6 +17,7 @@ public class AlgoEff {
     }
 
     public static void main(String[] args) {
-        System.out.println();
+        constantTime();
+        logarithmicTime(100);
     }
 }

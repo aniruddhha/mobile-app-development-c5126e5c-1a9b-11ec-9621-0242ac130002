1ec65d005bbc1d8418f9a47dea9779cd20730c46
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-22 11:07:19 +0530

    exception : variation

--- exceptions/Calc.java
+++ exceptions/Calc.java
@@ -15,12 +15,26 @@ public class Calc {
         return res;
     }
 
+    public static String div(int num1, int num2) {
+
+        String sts = "";
+        try {
+            // res = num2 / num1; // logical error - bug
+            sts = "" + (num1 / num2);// i think this line can break executioin
+        } catch (Exception e) {
+            sts = "you are dividing by zero";
+        }
+
+        return sts;
+    }
+
     public static void main(String[] args) {
 
         System.out.println("10, 20 = " + division(10, 20));
         System.out.println("100, 10 = " + division(100, 10));
         System.out.println("1, 1 = " + division(1, 1));
         System.out.println("78, 0 = " + (division(78, 0) == -1 ? "You are Dividing by Zero" : division(78, 0)));
+        System.out.println("100, 0 = " + div(100, 0));
         System.out.println("3, 2 = " + division(3, 2));
     }
 }

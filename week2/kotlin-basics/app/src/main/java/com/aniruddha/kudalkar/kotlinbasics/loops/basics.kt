469688b924377740fb02dc9ffa54b0c00cfc80a6
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-28 17:55:33 +0530

    loop : for variations

--- week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/loops/basics.kt
+++ week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/loops/basics.kt
@@ -1,15 +1,18 @@
 package com.aniruddha.kudalkar.kotlinbasics.loops
 
 fun demo1() {
+//    val i = 0
+//    val j = 10
+
     for (i in 1..10) { // count based
         println("i = $i")
     }
 
-    for (i in 10.downTo(0).step( 3)) { // normal
+    for (i in 10.downTo(0).step(3)) { // normal
         println("i = $i")
     }
 
-    for (i in 10 downTo 0 step 3 ) { // infix one
+    for (i in 10 downTo 0 step 3) { // infix one
         println("i = $i")
     }
 }
@@ -17,7 +20,7 @@ fun demo1() {
 fun demo2() {
     val nums = intArrayOf(56, 89, 23, 78, 65, 20, 12, 103)
 
-    for ( i in nums.indices ) {
+    for (i in nums.indices) {
         println(" index = $i, value = ${nums[i]}")
     }
 
@@ -25,8 +28,12 @@ fun demo2() {
         println("value = $value")
     }
 
-    for( (index, value) in nums.withIndex() ) {
+    for ((index, value) in nums.withIndex()) {
         println("Index ${index}, Value = $value")
+    }
+
+    for (i in 0 until nums.size - 2) {
+        println(i)
     }
 }
 

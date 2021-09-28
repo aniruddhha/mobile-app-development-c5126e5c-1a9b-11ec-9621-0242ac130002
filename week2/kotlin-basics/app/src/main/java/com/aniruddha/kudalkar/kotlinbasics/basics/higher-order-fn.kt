90650b69e854ab7bd5c046bdbcfd02a8a1b74bdd
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-28 15:02:25 +0530

    functions : higher order functions

--- week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/basics/higher-order-fn.kt
+++ week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/basics/higher-order-fn.kt
@@ -11,16 +11,20 @@ fun main() {
     }
 
     val fn2: () -> Int = { -> 10 } // same function but representation is different
+    val fn3: () -> Int = { 10 }
 
-    val fn3: (st: String) -> Int = fun(st: String): Int {
+    val fn4: (st: String) -> Int = fun(st: String): Int {
         return st.length
     }
 
-    val fn4: (st: String) -> Int = { st: String -> st.length }
+    val fn5: (st: String) -> Int = { st: String -> st.length }
+    val fn6: (st: String) -> Int = { st -> st.length }
+    val fn7: (st: String) -> Int = { st -> 10 }
+    val fn8: (st: String) -> Int = { 10 }
 
-    val fn5: (st: String, dt: Int, prc: Double) -> Int =
+    val fn9: (st: String, dt: Int, prc: Double) -> Int =
         fun(st: String, dt: Int, prc: Double): Int {
             return 10
         }
-    val fn6: (st: String, dt: Int, prc: Double) -> Int = { st, dt, prc  -> 10  }
+    val fn10: (st: String, dt: Int, prc: Double) -> Int = { st, dt, prc  -> 10  }
 }

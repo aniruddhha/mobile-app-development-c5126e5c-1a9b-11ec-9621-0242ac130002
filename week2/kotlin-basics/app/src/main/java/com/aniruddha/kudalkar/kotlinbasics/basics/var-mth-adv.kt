23c57478206d86484f43b1f3f9d50ed0d131d1f1
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-28 12:44:13 +0530

    functions : inner and anonymous function

--- week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/basics/var-mth-adv.kt
+++ week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/basics/var-mth-adv.kt
@@ -5,8 +5,7 @@ fun someObj() = object { // inline function returning object
     var os = "android"
 }
 
-fun main() {
-
+fun objDemo() {
     val ob = object {
         var dt = 10
         var os = "android"
@@ -16,4 +15,26 @@ fun main() {
 //    ob1.dt
 
     println( " Os ${ob.dt}")
+}
+
+fun fnDemo() {
+    val dt = 10 // accessible in inner function
+
+    val fn = fun(n1: Int, n2: Int): Int {
+        val amt = 45 // accessible only inside this function
+        return n1 * n2 + dt + amt
+    }
+
+//    println(amt)
+
+    println(fn)
+    println(fn(10, 20))
+
+    fun hi() {
+        println(" Dt is $dt")
+    }
+}
+
+fun main() {
+    fnDemo()
 }

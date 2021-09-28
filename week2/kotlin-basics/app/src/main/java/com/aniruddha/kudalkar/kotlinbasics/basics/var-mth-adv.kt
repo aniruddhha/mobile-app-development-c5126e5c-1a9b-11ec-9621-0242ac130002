2baafa27d443080d716a1d4ed8ba8018e6312b60
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-28 14:07:28 +0530

    functions : fn type

--- week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/basics/var-mth-adv.kt
+++ week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/basics/var-mth-adv.kt
@@ -14,7 +14,7 @@ fun objDemo() {
     var ob1 = someObj()
 //    ob1.dt
 
-    println( " Os ${ob.dt}")
+    println(" Os ${ob.dt}")
 }
 
 fun fnDemo() {
@@ -24,7 +24,7 @@ fun fnDemo() {
         println(" Dt is $dt")
     }
 
-    val fn: (num1 : Int, num2 : Int) -> Int = fun(n1: Int, n2: Int): Int { // inner anonymous function
+    val fn: (num1: Int, num2: Int) -> Int = fun(n1: Int, n2: Int): Int { // inner anonymous function
         val amt = 45 // accessible only inside this function
         return n1 * n2 + dt + amt
     }
@@ -36,13 +36,23 @@ fun fnDemo() {
     println(fn(10, 20))
 }
 
-fun outer() {
+fun outer(): (n1: Int) -> String {
     /*
         if variable holds integer value its type is Int, so
         if variable holds function value, what its type would be ?
      */
+
+    return fun(n: Int): String {
+        return "abc"
+    }
 }
 
 fun main() {
     fnDemo()
+
+    val fn = outer()
+    val str = fn(10)
+
+    println(fn) // what is output ?
+    println(str) // what is output ?
 }

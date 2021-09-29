595a88e718a24265eeec1726c164b9a2798523a2
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-29 15:38:19 +0530

    classes and objects : get and set

--- week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/clsobj/GtSt.kt
+++ week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/clsobj/GtSt.kt
@@ -1,21 +1,14 @@
 package com.aniruddha.kudalkar.kotlinbasics.clsobj
 
 class GtSt(
-    private var xPos : Int,
-    private var yPos : Int
+    private var xPos : Int
 ) {
     var x : Int
         get() = this.xPos
         set(value) { this.xPos = value }
-
-    var y: Int
-        get() = this.yPos
-        set(value) { this.yPos = value }
 }
 
 fun main() {
-    val gt1 = GtSt(10, 56) // `get()` is getting callled
-    gt1.x = 10 // `set(value)` is getting called
-    println("${gt1.x}")
-
+    val gt1 = GtSt(10)
+    gt1.x = 10
 }

04df4ca893d52c16cb2453da5c5554ed156452cd
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-29 16:00:39 +0530

    classes and objects : get and set

--- week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/clsobj/GtSt.kt
+++ week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/clsobj/GtSt.kt
@@ -11,4 +11,19 @@ class GtSt(
 fun main() {
     val gt1 = GtSt(10)
     gt1.x = 10
+
+    val a = Abc()
+    data = 50;
+
+    val sv = Visibility()
+    sv.v = 10
+    sv.z = 56
+
+    val sb = SubVisibility()
+    sb.v = 10
+    sb.z = 10
 }
+
+class SubVisibility : Visibility()
+
+
---
+++ week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/clsobj/visibility.kt
@@ -0,0 +1,24 @@
+package com.aniruddha.kudalkar.kotlinbasics.clsobj
+
+//Classes, objects, interfaces, constructors, and functions,
+// as well as properties and their setters, can have visibility modifiers.
+
+// There are four visibility modifiers in Kotlin: private, protected, internal, and public.
+// The default visibility is public.
+
+private fun hello() = "hi" //  visible inside this .kt file
+internal class Abc // visible inside the same module
+public var data = 56 // visible every where
+
+open class Visibility (
+    private var x : Int = 40,
+    protected var y : Int = 50,
+    internal var z :Int = 60,
+    public var v :Int = 70
+) {
+    // private - member is visible inside this class
+    // protected - same visibility as one marked as private,
+    // but that it is also visible in subclasses.
+    // internal - inside this module who sees the declaring class sees its internal members.
+    // public - who sees the declaring class sees its public members.
+}

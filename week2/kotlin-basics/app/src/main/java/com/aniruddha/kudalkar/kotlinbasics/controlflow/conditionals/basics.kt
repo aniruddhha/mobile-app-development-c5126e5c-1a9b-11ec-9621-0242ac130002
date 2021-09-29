5214fc55747fc6284d5bc0c1cbbd25474b3cf445
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-29 09:42:19 +0530

    safe commit

--- week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/controlflow/conditionals/basics.kt
+++ week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/controlflow/conditionals/basics.kt
@@ -1,20 +1,24 @@
 package com.aniruddha.kudalkar.kotlinbasics.controlflow.conditionals
 
 
+// for taking input you can use
+// - scanner from java
+// - use kotlins native feature readLine
+
 fun demo1() {
-    val os = readLine()
+    val os = readLine() // take input from user
 
     if(os.equals("10")) {
         println("Oreo")
     } else if (os.equals("9")) {
         println("Pie")
     } else {
-        print("Android")
+        println("Android")
     }
 }
 
 fun demo2() {
-    val os = readLine()
+    val os = readLine() // take input from user
     when(os) {
         "10" -> println("Oreo")
         "9" -> print("Pie")

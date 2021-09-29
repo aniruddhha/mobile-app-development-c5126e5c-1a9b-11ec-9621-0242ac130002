194a47a85f3cf2c3ca6d6210f8030d4aaac500c3
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-29 10:54:49 +0530

    safe push

---
+++ week2/kotlin-basics/app/build/generated/source/buildConfig/release/com/aniruddha/kudalkar/kotlinbasics/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.aniruddha.kudalkar.kotlinbasics;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.aniruddha.kudalkar.kotlinbasics";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/typeandcast/main.kt
@@ -0,0 +1,32 @@
+package com.aniruddha.kudalkar.kotlinbasics.typeandcast
+
+
+fun demo1() {
+    val age = 20
+    val str = "hello"
+    println(" is str a String ${str is String}" )
+    println(" is str not a String ${str !is String}" )
+    println(" is Age a Integer ${age is Int}")
+}
+
+fun demo2() {
+    //    val num1 = "100" as Int // as is unsafe cast operator
+//    val num2 = "200" as Int // unsafe wont work here
+
+    val stNum1 = "122"
+    val num1 = stNum1.toInt()
+    val stNum2 = "2323"
+    val num2 = stNum2.toInt()
+
+    val chr = 'c'
+    println("Ascii  ${chr.code}")
+
+    println("Num1 $num1, Num2 $num2")
+    println("Addition is ${num1 + num2}")
+}
+
+
+
+fun main() {
+    demo2()
+}

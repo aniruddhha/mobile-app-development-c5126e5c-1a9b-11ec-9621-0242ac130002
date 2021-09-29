4b0011c53de207656680c128209a68fd24192a88
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-29 11:10:43 +0530

    operators

--- week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/operators/basics.kt
+++ week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/operators/basics.kt
@@ -27,25 +27,5 @@ fun demo1() {
 
 fun main() {
 
-    var i = 10
-    var j = 20
-
-    println("i + j ${i + j}")
-    println("i - j ${i - j}")
-    println("i * j ${i * j}")
-    println("i / j ${i / j}")
-    println("i == j ${i == j}")
-    println("i != j ${i != j}")
-    println("i++ ${ i++}")
-    println(" --i ${ --i}")
-    println("i & j ${ i.and(j)}")
-    println("i & j ${ i and j}")
-    println("i | j ${ i or j}")
-    println("i ^ j ${ i xor j}")
-    println("i && j ${ (i > j) && (i > 50) }")
-    println("i || j ${ (i < j) || (i > 10) }")
-    println("i <= j ${ (i <= j)}")
-    println("i >= j ${ (i >= j)}")
-    println("Not ${ !(i == j)}")
-    println("Not ${ (i == j).not()}")
+    demo1()
 }

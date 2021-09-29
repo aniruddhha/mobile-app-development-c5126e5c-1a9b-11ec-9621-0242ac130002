15712239e681cd7e20fb861cd07675bfed8ac9d7
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-29 14:44:06 +0530

    classes and objects : overriding

--- week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/clsobj/inheritance.kt
+++ week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/clsobj/inheritance.kt
@@ -1,12 +1,18 @@
 package com.aniruddha.kudalkar.kotlinbasics.clsobj
 
 open class Config { // by default every class extends `Any` class
+
+    open val pool : Int = 12
+
     open fun doConfiguration() {
         println("Inside Config class")
     }
 }
 
 class DbConfig : Config() { // dbconfig is a config
+
+    override val pool = 56
+
     override fun doConfiguration() {
         println("Inside Db Configuration")
         // code related to db
@@ -15,6 +21,7 @@ class DbConfig : Config() { // dbconfig is a config
 
 class FileConfig : Config() { // fileconfig is a config
     override fun doConfiguration() {
+
         println("Inside File Configuration")
         // code related to file
     }
@@ -22,12 +29,21 @@ class FileConfig : Config() { // fileconfig is a config
 
 fun main() {
 
-    val cfg1 = Config()
+    val cfg1: Config = Config()
     cfg1.doConfiguration()
 
-    val cfg2 = DbConfig()
+    val cfg2: DbConfig = DbConfig()
     cfg2.doConfiguration()
 
-    val cfg3 = FileConfig()
+    val cfg3: FileConfig = FileConfig()
     cfg3.doConfiguration()
+
+    val i: Int = 10
+    val cfg4: Config = DbConfig()
+    cfg4.doConfiguration() // -> output ? -> Inside Db Configuration
+    val cfg5: Config = FileConfig()
+    cfg5.doConfiguration() // -> output ? -> Inside File Configuration
+
+    //val cfg6: DbConfig = Config() //-> wont work
+    //val cfg7: FileConfig = Config() //-> wont work
 }

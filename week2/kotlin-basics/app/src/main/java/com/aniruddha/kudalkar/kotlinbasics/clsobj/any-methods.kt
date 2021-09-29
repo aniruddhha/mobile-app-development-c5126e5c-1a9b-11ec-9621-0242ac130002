a164c71af34ce8e3bdcc0673a14cc073e6cfc35a
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-29 16:57:32 +0530

    classes and objects : toString and equality

---
+++ week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/clsobj/any-methods.kt
@@ -0,0 +1,27 @@
+package com.aniruddha.kudalkar.kotlinbasics.clsobj
+
+class Dv(
+    private val x : Int,
+    private val y : Int
+)  {
+    //Returns a string representation of the object.
+    override fun toString() = " x = $x, y = $y "
+
+    override fun equals(other: Any?): Boolean {
+        val oth = other as Dv // type casting
+        return (this.x == oth.x) && (this.y == oth.y)
+    }
+}
+
+fun main() {
+    // obj = values of state
+    // dv1 = values of x & y
+    // dv2 = values of x & y
+
+    val dv1 = Dv(10, 465)
+
+    val dv2 = Dv(10, 465)
+
+    println( dv1.equals(dv2))
+    println( dv2.equals(dv1))
+}
---
+++ week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/clsobj/more-on-cls.kt
@@ -0,0 +1,2 @@
+package com.aniruddha.kudalkar.kotlinbasics.clsobj
+

fcf4cf82da512c7166307c4b9fe9804a76a80054
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-29 10:09:03 +0530

    conditional : when variations

--- week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/controlflow/conditionals/basics.kt
+++ week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/controlflow/conditionals/basics.kt
@@ -3,7 +3,7 @@ package com.aniruddha.kudalkar.kotlinbasics.controlflow.conditionals
 
 // for taking input you can use
 // - scanner from java
-// - use kotlins native feature readLine
+// - use kotlins native feature readLine -> you read string
 
 fun demo1() {
     println("Enter The OS : ")
@@ -28,6 +28,31 @@ fun demo2() {
     }
 }
 
-fun main() {
+fun demo3() {
+    println("Enter Name")
+    val nm = readLine()
 
+    val info = if(nm.equals("android")) 11 else 30
+    val dt = when(nm){
+         "123" -> 123
+          else -> 23
+    }
+
+    println("Info $info, Dt $dt")
+}
+
+fun demo4() {
+    val safeNums = intArrayOf(45, 89, 50)
+
+    val num = 10
+
+    when(num) {
+        in 1..10 -> println("it is in range")
+        !in 96..100 -> println("it is not in safe range")
+        in safeNums -> println("it is in safe range")
+        else -> println("invalid number")
+    }
+}
+
+fun main() {
 }

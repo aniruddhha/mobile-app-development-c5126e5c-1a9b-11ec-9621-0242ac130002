8f3fb036aa849d1fcf04236d6520b685e8311bd8
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-30 11:59:55 +0530

    classes and objects : toString and equality

--- week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/clsobj/any-methods.kt
+++ week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/clsobj/any-methods.kt
@@ -17,7 +17,7 @@ class Dv(
     }
 }
 
-fun main() {
+private fun demo20() {
     // obj = values of state
     // dv1 = values of x & y
     // dv2 = values of x & y
@@ -28,4 +28,38 @@ fun main() {
 
     println(dv1.equals(dv2))
     println(dv2.equals(dv1))
+}
+
+private fun demo23() {
+
+    // For values represented by primitive types at runtime (for example, Int),
+    // the === equality check is equivalent to the == check.
+
+
+    val dv1 = Dv(11,20)
+    val dv2 = Dv(10, 20)
+
+    println( "Dv == ${dv1 == dv2}" )
+    println( "Dv === ${dv1 === dv2}" )
+
+    val i1 = 10
+    val i2 = 20
+
+    println( "i == ${i1 == i2}" )
+    println( "i === ${i1 === i2}" )
+
+    val s1 = "abc" // this initialization brings string from common memory area
+    val s2 = "abc" // this initialization brings string from common memory area
+
+    println(" S1 == ${s1 == s2}") // structural equality - checks content
+    println(" S1 === ${s1 === s2}") // referential equality - check memory references
+
+    val st1 = String("hi".toByteArray()) // new memory reference
+    val st2 = String("hi".toByteArray()) // // new memory reference
+
+    println(" St1 == ${st1 == st2}") // structural equality - checks content
+    println(" St1 === ${st1 === st2}") // referential equality - check memory references
+}
+fun main() {
+    demo23()
 }

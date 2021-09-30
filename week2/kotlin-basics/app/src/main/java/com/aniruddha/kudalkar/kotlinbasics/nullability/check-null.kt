e627fd93b562d224d6add23abde87fe1d8ce0554
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-30 11:35:39 +0530

    classes and objects : toString and equality

--- week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/nullability/check-null.kt
+++ week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/nullability/check-null.kt
@@ -3,6 +3,7 @@ package com.aniruddha.kudalkar.kotlinbasics.nullability
 import java.util.*
 
 private fun demo1() {
+
     var dt: Date? = null // it can be null or non null
 
     if(System.currentTimeMillis() > 5988) { // random condition
@@ -24,6 +25,23 @@ private fun demo1() {
     }
 }
 
+private fun demo2() {
+    var dt: Array<String>? = null
+
+    if(System.currentTimeMillis() > 5988) { // random condition
+        dt = arrayOf("sdlkfjsldkf", "slkfkdsdklf", "dslfjslkdfj")
+    }
+
+    val ln = if(dt != null) dt.size else -3
+
+    // If the expression to the left of ?: is not null,
+    // the Elvis operator returns it, otherwise
+    // it returns the expression to the right.
+    val ln1 = dt?.size ?: -1 // ?: is elvis operator , kotlins way
+
+    println("Length is ${ln}")
+    println("Length is ${ln1}")
+}
 fun main() {
 
 }

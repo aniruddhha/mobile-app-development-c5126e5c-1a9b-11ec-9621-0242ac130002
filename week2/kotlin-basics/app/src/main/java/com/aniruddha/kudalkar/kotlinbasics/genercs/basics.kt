6a090501690f8f08705f6601bfb097671e1ceebd
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-30 12:52:00 +0530

    classes and objects : generics

--- week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/genercs/basics.kt
+++ week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/genercs/basics.kt
@@ -4,10 +4,15 @@ import java.util.Date
 
 private class Box<in T, out U>(
     private val t : T,
-    private val u : U
+    private var u : U
 ) {
 
-    fun calc(t : T)  { // t used as input to the function
+    fun calc(t : T) : U  { // t used as input to the function
+        if( (t is Int) && (u is Int)) {
+            u = (t * t) as U
+            return u
+        }
+        return  u
     }
 
     fun getDu() : U { // u used to output/return from the function
@@ -19,4 +24,6 @@ fun main() {
     val box1: Box<Int, Double> = Box(10, 56.9)
     val box2 = Box<String, Boolean>("abc", true)
     val box3 = Box('c', Date())
+
+    println(Box(10, 2).calc(5))
 }

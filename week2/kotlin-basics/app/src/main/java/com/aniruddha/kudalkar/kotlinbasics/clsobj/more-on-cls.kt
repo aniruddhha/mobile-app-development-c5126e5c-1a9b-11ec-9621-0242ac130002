0a55d20aec01fac6f0fa98f31d5fb42272ef1b46
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-09-30 12:11:51 +0530

    classes and objects : data classes

--- week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/clsobj/more-on-cls.kt
+++ week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/clsobj/more-on-cls.kt
@@ -1,2 +1,18 @@
 package com.aniruddha.kudalkar.kotlinbasics.clsobj
 
+/*
+It is not unusual to create classes whose main purpose is to hold data.
+In such classes, some standard functionality and some utility functions are often
+mechanically derivable from the data.
+In Kotlin, these are called data classes and are marked with data
+ */
+private data class MsInfo(
+    val nm: String = "abc",
+    val age: Int = 20,
+    var isNri : Boolean = false
+)
+
+fun main() {
+    val d1 = MsInfo()
+    println(d1.toString())
+}

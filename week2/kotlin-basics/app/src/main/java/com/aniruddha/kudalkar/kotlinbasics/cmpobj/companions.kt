1776f738845c1f025f999ece8eb116d7b4b1734f
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-01 09:55:20 +0530

    object : companion object

--- week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/cmpobj/companions.kt
+++ week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/cmpobj/companions.kt
@@ -1,17 +1,34 @@
 package com.aniruddha.kudalkar.kotlinbasics.cmpobj
 
 class DbConnector {
-    companion object  {
+
+    private val nm = "nnn"
+    val dbNm = "nnn"
+
+    companion object {
         val url = "http://abc.com"
         val userName = "acc"
         val PASSWORD = "acbv"
+
+        fun crackPassword() {
+            //nm = 10 private member of class
+            // dbNm = "" nothing is accessible from class
+        }
+    }
+
+    fun connect() {
+        println("Accessing URL $url")
+
+        crackPassword()
     }
 }
 
 fun main() {
     val dc = DbConnector() // not needed
+    dc.connect()
 
     println("Url - ${DbConnector.url}")
     println("User Name - ${DbConnector.userName}")
     println("Password - ${DbConnector.PASSWORD}")
+    DbConnector.crackPassword()
 }

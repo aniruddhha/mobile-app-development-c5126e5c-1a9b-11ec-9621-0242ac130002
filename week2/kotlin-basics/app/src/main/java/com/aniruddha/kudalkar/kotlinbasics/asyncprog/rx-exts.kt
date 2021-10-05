608c2d5b73750933684d3d5f004f783f4b3c17ab
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-05 09:38:33 +0530

    rxkotlin : operators

--- week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/asyncprog/rx-exts.kt
+++ week2/kotlin-basics/app/src/main/java/com/aniruddha/kudalkar/kotlinbasics/asyncprog/rx-exts.kt
@@ -3,6 +3,7 @@ package com.aniruddha.kudalkar.kotlinbasics.asyncprog
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 /*
 implementation 'io.reactivex.rxjava3:rxkotlin:3.0.0'
@@ -10,6 +11,7 @@ implementation 'io.reactivex.rxjava3:rxandroid:3.0.0'
 
  */
 private fun obs1() {
+    // cold and hot
     val obs = Observable.create<String> {
         // this loop is simulation of going to fb
         // and pulling data back
@@ -34,6 +36,22 @@ private fun obs1() {
         .subscribe()
 }
 
+private fun obs2() {
+    Observable.interval(1300, TimeUnit.MILLISECONDS)
+        .subscribeOn(Schedulers.computation())
+        .doOnNext { println(it) }
+//        .doOnError {  }
+//        .doOnComplete {  }
+        .subscribe()
+
+    /*Observable.interval(1300, TimeUnit.MILLISECONDS)
+        .subscribe(
+            { }, // doOnNext
+            { },  // doOnError
+            { } // doOnComplete
+        )*/
+}
+
 fun main() {
-    obs1()
+    obs2()
 }

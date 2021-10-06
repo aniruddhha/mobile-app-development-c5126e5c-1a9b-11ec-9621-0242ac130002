cd1d1eee7ce9ea7e507dc753c8ea1a38ff7b465c
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-06 11:14:04 +0530

    android : color picker demo - logic

--- week3/android-basics/app/src/main/java/com/aniruddha/kudalkar/androidbasics/ColorPickerActivity.kt
+++ week3/android-basics/app/src/main/java/com/aniruddha/kudalkar/androidbasics/ColorPickerActivity.kt
@@ -1,11 +1,34 @@
 package com.aniruddha.kudalkar.androidbasics
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class ColorPickerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_color_picker)
+
+
+        val kafhskafhdslkhfd = findViewById<Button>(R.id.btPk)
+        kafhskafhdslkhfd.setOnClickListener {
+
+            val etRd = findViewById<EditText>(R.id.etRd)
+            val red = etRd.text.toString().toInt()
+
+            val etGrn = findViewById<EditText>(R.id.etGrn)
+            val green = etGrn.text.toString().toInt()
+
+            val etBlu = findViewById<EditText>(R.id.etBlu)
+            val blue = etBlu.text.toString().toInt()
+
+            val txtClr = findViewById<TextView>(R.id.txtClr)
+
+            txtClr.setTextColor(Color.rgb(red, green, blue))
+        }
     }
 }

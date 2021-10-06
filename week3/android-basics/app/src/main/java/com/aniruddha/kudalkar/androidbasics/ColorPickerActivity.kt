ae098783cfcac930a79c774c7a018b0e62f6ec07
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-06 10:23:39 +0530

    android : view hierarchy and activity

---
+++ week3/android-basics/app/src/main/java/com/aniruddha/kudalkar/androidbasics/ColorPickerActivity.kt
@@ -0,0 +1,11 @@
+package com.aniruddha.kudalkar.androidbasics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ColorPickerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_color_picker)
+    }
+}

e1405808213833c082df08f85f8c5f7d7a669552
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-07 14:38:26 +0530

    populating data

--- week3/android-basics/app/src/main/java/com/aniruddha/kudalkar/androidbasics/InvoiceActivity.kt
+++ week3/android-basics/app/src/main/java/com/aniruddha/kudalkar/androidbasics/InvoiceActivity.kt
@@ -2,10 +2,26 @@ package com.aniruddha.kudalkar.androidbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import java.time.LocalDate
+import java.util.*
+import kotlin.random.Random
 
 class InvoiceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_invoice)
+
+        populateInvoiceDetails()
+    }
+
+    private fun populateInvoiceDetails() {
+
+        findViewById<TextView>(R.id.txtInvNum).text = "#${Random(233).nextInt()}"
+        findViewById<TextView>(R.id.txtClNm).text = "Abc Pvt Ltd"
+        findViewById<TextView>(R.id.txtClAddr).text = "#122, abc street, pqr road."
+
+        findViewById<TextView>(R.id.txtInvDt).text = "13-01-2021"
+        findViewById<TextView>(R.id.txtInvAmt).text = "INR. 20317"
     }
 }

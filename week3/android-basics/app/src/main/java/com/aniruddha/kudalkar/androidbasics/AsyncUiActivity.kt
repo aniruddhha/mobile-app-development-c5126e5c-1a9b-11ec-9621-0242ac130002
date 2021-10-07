a6548d8a12caec9a856a73c62ed16471f9876abc
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-07 16:27:54 +0530

    use of co-routine in app : custom scope

--- week3/android-basics/app/src/main/java/com/aniruddha/kudalkar/androidbasics/AsyncUiActivity.kt
+++ week3/android-basics/app/src/main/java/com/aniruddha/kudalkar/androidbasics/AsyncUiActivity.kt
@@ -4,19 +4,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.Flow
+import org.w3c.dom.Text
 
 class AsyncUiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_async_ui)
-
         val txtCnt = findViewById<TextView>(R.id.txtCnt)
 
-        GlobalScope.launch {
+        usingCustomScope(txtCnt)
+    }
+
+    private fun usingCustomScope(txt: TextView) {
+        val scp = CoroutineScope(Job() + Dispatchers.IO)
+        scp.launch {
             (60 downTo 1).map {
                 delay(1000)
                 runOnUiThread {
-                    txtCnt.text = "$it"
+                    txt.text = "$it"
+                }
+            }
+        }
+    }
+
+    private fun usingGlobalScope(txt: TextView) {
+        GlobalScope.launch { // global scopes you have to avoid
+            (60 downTo 1).map {
+                delay(1000)
+                runOnUiThread {
+                    txt.text = "$it"
                 }
             }
         }

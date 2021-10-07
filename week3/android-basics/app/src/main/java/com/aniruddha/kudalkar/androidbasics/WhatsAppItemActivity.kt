cb803dadbe57b9a01aa85f9e26d88679a0d5daee
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-07 09:58:10 +0530

    material design : card view and chips

---
+++ week3/android-basics/app/src/main/java/com/aniruddha/kudalkar/androidbasics/WhatsAppItemActivity.kt
@@ -0,0 +1,11 @@
+package com.aniruddha.kudalkar.androidbasics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WhatsAppItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_whats_app_item)
+    }
+}

dfdab1a2db338f75a0f81c3a1f3042721135896f
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-08 17:12:30 +0530

    chat activity : adding chat bubbles to layout fine tuning

--- week3/android-basics/app/src/main/java/com/aniruddha/kudalkar/androidbasics/ChatActivity.kt
+++ week3/android-basics/app/src/main/java/com/aniruddha/kudalkar/androidbasics/ChatActivity.kt
@@ -23,9 +23,15 @@ class ChatActivity : AppCompatActivity() {
 
         val inflater = LayoutInflater.from(this)
         val vwRt = inflater.inflate(R.layout.chat_right_item, llChats, false)
+        vwRt.findViewById<TextView>(R.id.txRtUsr).text = "Abc"
+        vwRt.findViewById<TextView>(R.id.txRtTm).text = "10:00 AM"
+        vwRt.findViewById<TextView>(R.id.txRtMsg).text = "WhatsApp Messenger, or simply WhatsApp, is an American freeware, cross-platform centralized instant messaging (IM) and voice-over-IP (VoIP) service owned by Facebook, Inc.[12] It allows users to send text messages and voice messages,[13] make voice and video calls, and share images, documents, user locations, and other content.[14][15] WhatsApp's client application runs on mobile devices but is also accessible from desktop computers, as long as the user's mobile device remains connected to the Internet while they use the desktop app.[16] The service requires a cellular mobile telephone number to sign up.[17] In January 2018, WhatsApp released a standalone business app targeted at small business owners, called WhatsApp Business, to allow companies to communicate with customers who use the standard WhatsApp client"
         llChats.addView(vwRt)
 
         val vwLt = inflater.inflate(R.layout.chat_left_item, llChats, false)
+        vwLt.findViewById<TextView>(R.id.txLtUsr).text = "Pqr"
+        vwLt.findViewById<TextView>(R.id.txLtTm).text = "11:00 AM"
+        vwLt.findViewById<TextView>(R.id.txLtMsg).text = "WhatsApp Messenger, or simply WhatsApp, is an American freeware, cross-platform centralized instant messaging (IM) and voice-over-IP (VoIP) service owned by Facebook, Inc.[12] It allows users to send text messages and voice messages,[13] make voice and video calls, and share images, documents, user locations, and other content.[14][15] WhatsApp's client application runs on mobile devices but is also accessible from desktop computers, as long as the user's mobile device remains connected to the Internet while they use the desktop app.[16] The service requires a cellular mobile telephone number to sign up.[17] In January 2018, WhatsApp released a standalone business app targeted at small business owners, called WhatsApp Business, to allow companies to communicate with customers who use the standard WhatsApp client"
         llChats.addView(vwLt)
     }
 

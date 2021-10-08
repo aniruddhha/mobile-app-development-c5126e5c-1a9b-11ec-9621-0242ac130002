9e527d4acca210d6e3e67df0f604a6db54dfb5ea
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-08 10:20:49 +0530

    seek bar implementation

--- week3/android-basics/app/src/main/java/com/aniruddha/kudalkar/androidbasics/DependentUiActivity.kt
+++ week3/android-basics/app/src/main/java/com/aniruddha/kudalkar/androidbasics/DependentUiActivity.kt
@@ -2,10 +2,30 @@ package com.aniruddha.kudalkar.androidbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.TypedValue
+import android.widget.SeekBar
+import android.widget.TextView
 
 class DependentUiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dependent_ui)
+
+        val txt = findViewById<TextView>(R.id.txt)
+
+        val skBr = findViewById<SeekBar>(R.id.skBr)
+        skBr.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                Log.i("@ani", "$progress")
+                txt.setTextSize(progress.toFloat())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
     }
 }

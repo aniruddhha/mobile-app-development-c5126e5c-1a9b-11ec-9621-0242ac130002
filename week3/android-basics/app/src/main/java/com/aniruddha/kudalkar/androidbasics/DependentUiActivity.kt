bc16a960c0ce05a60d2f488a81200e8ac0592163
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-08 11:45:09 +0530

    edittext change listener : traditional way

--- week3/android-basics/app/src/main/java/com/aniruddha/kudalkar/androidbasics/DependentUiActivity.kt
+++ week3/android-basics/app/src/main/java/com/aniruddha/kudalkar/androidbasics/DependentUiActivity.kt
@@ -2,6 +2,8 @@ package com.aniruddha.kudalkar.androidbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.util.TypedValue
 import android.widget.EditText
@@ -14,6 +16,10 @@ class DependentUiActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dependent_ui)
 
+        watchingTextTraditionalWay()
+    }
+
+    private fun seekBarChangeEvents() {
         val txt = findViewById<TextView>(R.id.txt)
 
         val skBr = findViewById<SeekBar>(R.id.skBr)
@@ -29,14 +35,34 @@ class DependentUiActivity : AppCompatActivity() {
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
             }
         })
+    }
 
+    private fun watchingTextKotlinsWay() {
+        val txt = findViewById<TextView>(R.id.txt)
         val et = findViewById<EditText>(R.id.etSz)
-
         et.doOnTextChanged { text, _, _, _ ->
             Log.i("@ani", "Text is $text")
             if(text.toString().isEmpty().not())
                 txt.textSize = text.toString().toFloat()
             else txt.setTextSize(1f)
         }
+    }
+
+    private fun watchingTextTraditionalWay() {
+        val txt = findViewById<TextView>(R.id.txt)
+        val et = findViewById<EditText>(R.id.etSz)
+
+        et.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(text: CharSequence?, start: Int, before: Int, count: Int) {
+                if(text.toString().isEmpty().not())
+                    txt.textSize = text.toString().toFloat()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
     }
 }

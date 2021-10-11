2d1f809d91c53f9408d781d29433d1e933fea861
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-11 11:00:02 +0530

    jetpack compose : modifier

--- week4/compose-basics/app/src/main/java/com/aniruddha/kudalkar/composebasics/MainActivity.kt
+++ week4/compose-basics/app/src/main/java/com/aniruddha/kudalkar/composebasics/MainActivity.kt
@@ -3,29 +3,13 @@ package com.aniruddha.kudalkar.composebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Text
-import androidx.compose.ui.res.painterResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContent {
-            Column {
-                Row {
-                    Image(
-                        painter = painterResource(R.drawable.ic_android),
-                        contentDescription = "Android Icons"
-                    )
-                    Text(
-                        text = "Hello World"
-                    )
-                }
-                Text(text = "This is detailed")
-            }
+            Demos.ModifierDemo()
         }
     }
 }
--- week4/compose-basics/app/src/main/java/com/aniruddha/kudalkar/composebasics/ui.kt
+++ week4/compose-basics/app/src/main/java/com/aniruddha/kudalkar/composebasics/ui.kt
@@ -1,9 +1,53 @@
 package com.aniruddha.kudalkar.composebasics
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
-@Composable
-fun SimpleText() {
-    Text(text = "Welcome To Jetpack Compose")
+class Demos {
+    companion object {
+
+        @Composable
+        fun SimpleText() {
+            Text(text = "Welcome To Jetpack Compose")
+        }
+
+        @Composable
+        fun RowsColumns() {
+            Column {
+                Row {
+                    Image(
+                        painter = painterResource(R.drawable.ic_android),
+                        contentDescription = "Android Icons"
+                    )
+                    Text(
+                        text = "Hello World"
+                    )
+                }
+                Text(text = "This is detailed")
+            }
+        }
+
+        @Composable
+        fun ModifierDemo() {
+            Text(
+                text = "Hello",
+
+                modifier = Modifier
+                    .width(100.dp)
+                    .height(100.dp)
+                    .padding(10.dp)
+//                    .fillMaxWidth()
+//                    .fillMaxHeight()
+                    .border(2.dp, Color.Black)
+            )
+        }
+    }
 }
+

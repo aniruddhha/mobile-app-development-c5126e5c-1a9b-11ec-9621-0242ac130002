9685a0a4dd3a401aa88b258b16ba9891be48ddaa
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-12 15:08:57 +0530

    fragment : case study

---
+++ week4/jetpack-apps/app/src/main/java/com/aniruddha/kudalkar/jetpackapps/fragment/BasicInstaActivity.kt
@@ -0,0 +1,12 @@
+package com.aniruddha.kudalkar.jetpackapps.fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aniruddha.kudalkar.jetpackapps.R
+
+class BasicInstaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basic_insta)
+    }
+}

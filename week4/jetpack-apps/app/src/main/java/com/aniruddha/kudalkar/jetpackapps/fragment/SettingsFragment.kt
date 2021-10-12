d49b946c3ad25e67dec26d9e90d0de149d0ec07e
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-12 15:27:26 +0530

    fragment : case study - fragments added

---
+++ week4/jetpack-apps/app/src/main/java/com/aniruddha/kudalkar/jetpackapps/fragment/SettingsFragment.kt
@@ -0,0 +1,10 @@
+package com.aniruddha.kudalkar.jetpackapps.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.aniruddha.kudalkar.jetpackapps.R
+
+class SettingsFragment : Fragment()

9f3d6eedf062d6cd93554b56613e043f85bca83d
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-12 15:53:11 +0530

    fragment : case study - overriden onCreateView

--- week4/jetpack-apps/app/src/main/java/com/aniruddha/kudalkar/jetpackapps/fragment/BasicInstaActivity.kt
+++ week4/jetpack-apps/app/src/main/java/com/aniruddha/kudalkar/jetpackapps/fragment/BasicInstaActivity.kt
@@ -2,11 +2,32 @@ package com.aniruddha.kudalkar.jetpackapps.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.aniruddha.kudalkar.jetpackapps.R
 
 class BasicInstaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_basic_insta)
+
+        findViewById<ImageView>(R.id.imgMsg).setOnClickListener {
+            supportFragmentManager.commit {
+                replace<MessagesFragment>(R.id.fragInsta )
+            }
+        }
+
+        findViewById<ImageView>(R.id.imgPrf).setOnClickListener {
+            supportFragmentManager.commit {
+                replace<ProfileFragment>(R.id.fragInsta)
+            }
+        }
+
+        findViewById<ImageView>(R.id.imgSet).setOnClickListener {
+            supportFragmentManager.commit {
+                replace<SettingsFragment>(R.id.fragInsta)
+            }
+        }
     }
 }

97af32e618e8b61233d2eb3de097499f8696f682
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-13 15:26:43 +0530

    lifecycle aware : livedata and view model

--- week4/jetpack-apps/app/src/main/java/com/aniruddha/kudalkar/jetpackapps/archcomp/LvDtVmViewModel.kt
+++ week4/jetpack-apps/app/src/main/java/com/aniruddha/kudalkar/jetpackapps/archcomp/LvDtVmViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class LvDtVmViewModel: ViewModel() {
+
     val prg : MutableLiveData<String> = MutableLiveData("5665")
 
     fun onPrgCh( pr : Int) {

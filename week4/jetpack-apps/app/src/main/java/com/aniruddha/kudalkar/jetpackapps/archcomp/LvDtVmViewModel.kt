57229ce4d3ab9a4d1f69d1b30201256d7c44f721
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-13 14:59:56 +0530

    lifecycle aware : livedata and view model

---
+++ week4/jetpack-apps/app/src/main/java/com/aniruddha/kudalkar/jetpackapps/archcomp/LvDtVmViewModel.kt
@@ -0,0 +1,12 @@
+package com.aniruddha.kudalkar.jetpackapps.archcomp
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LvDtVmViewModel: ViewModel() {
+    val prg : MutableLiveData<String> = MutableLiveData("5665")
+
+    fun onPrgCh( pr : Int) {
+        prg.value = "$pr"
+    }
+}
---
+++ week4/jetpack-apps/app/src/main/java/com/aniruddha/kudalkar/jetpackapps/archcomp/LvDtVwMdlActivity.kt
@@ -0,0 +1,34 @@
+package com.aniruddha.kudalkar.jetpackapps.archcomp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.aniruddha.kudalkar.jetpackapps.R
+import com.aniruddha.kudalkar.jetpackapps.databinding.ActivityLvDtVwMdlBinding
+
+class LvDtVwMdlActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding: ActivityLvDtVwMdlBinding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_lv_dt_vw_mdl
+        )
+
+        val viewModel: LvDtVmViewModel by viewModels()
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
+    }
+}
+
+abstract class Animal
+
+class Tiger : Animal() {
+    fun hiTiger() { }
+}
+
+fun diff() {
+    val an: Animal = Tiger()
+//    an.hiTiger()
+}

0371cc94ad3592753091983a4528055ad78d4361
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-14 16:46:27 +0530

    case study : Stock App - login navigation

--- week4/jetpack-apps/app/src/main/java/com/aniruddha/kudalkar/jetpackapps/stockapp/StockAppActivity.kt
+++ week4/jetpack-apps/app/src/main/java/com/aniruddha/kudalkar/jetpackapps/stockapp/StockAppActivity.kt
@@ -2,6 +2,9 @@ package com.aniruddha.kudalkar.jetpackapps.stockapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import com.aniruddha.kudalkar.jetpackapps.R
@@ -10,6 +13,15 @@ class StockAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stock_app)
+
+        loadLogin()
+
+        val vm : StockViewModel by viewModels()
+        vm.loginCheck.observe(this) {
+            Log.i("@ani", "State is $it")
+            if(it == 100) loadStocks()
+            else Toast.makeText(this, "Invalid Login", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun loadLogin() {
--- week4/jetpack-apps/app/src/main/java/com/aniruddha/kudalkar/jetpackapps/stockapp/StockViewModel.kt
+++ week4/jetpack-apps/app/src/main/java/com/aniruddha/kudalkar/jetpackapps/stockapp/StockViewModel.kt
@@ -1,11 +1,24 @@
 package com.aniruddha.kudalkar.jetpackapps.stockapp
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class StockViewModel: ViewModel() {
 
+    val loginCheck : MutableLiveData<Int> = MutableLiveData()
+
+    val userName : MutableLiveData<String> = MutableLiveData()
+    val password : MutableLiveData<String> = MutableLiveData()
+
     fun onLogin() {
         Log.i("@ani", "User Logged In")
+        Log.i("@ani", "User Name - ${userName.value}")
+        Log.i("@ani", "Password - ${password.value}")
+        if(userName.value == "android" && password.value == "android") {
+            loginCheck.value = 100
+        }else {
+            loginCheck.value = -100
+        }
     }
 }

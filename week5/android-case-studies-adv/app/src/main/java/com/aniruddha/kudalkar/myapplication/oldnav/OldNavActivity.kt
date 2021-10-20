b0ffc0466980cc0e422d445e60a20620859fb8dc
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-20 10:11:43 +0530

    jetpack navigation : created fragments

--- week5/android-case-studies-adv/app/src/main/java/com/aniruddha/kudalkar/myapplication/oldnav/OldNavActivity.kt
+++ week5/android-case-studies-adv/app/src/main/java/com/aniruddha/kudalkar/myapplication/oldnav/OldNavActivity.kt
@@ -24,7 +24,7 @@ class OldNavActivity : AppCompatActivity() {
     fun goToMainFragment() {
         Log.i("@ani", "Backstack Count ${supportFragmentManager.backStackEntryCount}")
         for(i in 0..supportFragmentManager.backStackEntryCount) {
-            supportFragmentManager.popBackStack()
+
         }
     }
 }

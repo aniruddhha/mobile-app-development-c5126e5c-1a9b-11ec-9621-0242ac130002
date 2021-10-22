c3aaaf1fc7150e0bd9bad5fa24539a1e18923b74
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-22 18:14:05 +0530

    shape drawable

--- week5/jetpack-navigation-graph/app/src/main/java/com/aniruddha/kudalkar/jetpacknavigationgraph/stlthm/StlThmActivity.kt
+++ week5/jetpack-navigation-graph/app/src/main/java/com/aniruddha/kudalkar/jetpacknavigationgraph/stlthm/StlThmActivity.kt
@@ -2,11 +2,18 @@ package com.aniruddha.kudalkar.jetpacknavigationgraph.stlthm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.aniruddha.kudalkar.jetpacknavigationgraph.R
 
 class StlThmActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stl_thm)
+
+        val drawable = ContextCompat.getDrawable(this, R.drawable.ic_blb_off)
+
+        val srt = resources.getString(R.string.app_name)
+
+        val col = ContextCompat.getColor(this, R.color.smTxBk)
     }
 }

7ecac98c7c984e65c0da4fa8d942489d912469a2
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-22 17:41:42 +0530

    styles and themes

---
+++ week5/jetpack-navigation-graph/app/src/main/java/com/aniruddha/kudalkar/jetpacknavigationgraph/stlthm/StlThmActivity.kt
@@ -0,0 +1,12 @@
+package com.aniruddha.kudalkar.jetpacknavigationgraph.stlthm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aniruddha.kudalkar.jetpacknavigationgraph.R
+
+class StlThmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stl_thm)
+    }
+}

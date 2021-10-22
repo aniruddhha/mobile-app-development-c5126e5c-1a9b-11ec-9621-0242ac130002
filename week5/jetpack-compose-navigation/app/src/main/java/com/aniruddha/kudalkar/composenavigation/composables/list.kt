db84ecdbd3921785aa1e07d5480cd861d459093f
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-22 10:19:14 +0530

    case study : bulb case study

--- week5/jetpack-compose-navigation/app/src/main/java/com/aniruddha/kudalkar/composenavigation/composables/list.kt
+++ week5/jetpack-compose-navigation/app/src/main/java/com/aniruddha/kudalkar/composenavigation/composables/list.kt
@@ -1,12 +1,15 @@
 package com.aniruddha.kudalkar.composenavigation.composables
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
 import androidx.compose.material.Switch
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 data class DataItem(
     var from : String,
@@ -28,13 +31,33 @@ fun DataList(
 
 @Composable
 fun DataRow(itm : DataItem) {
-    Row() {
-        Column() {
+    Row(
+        Modifier
+            .height(80.dp)
+            .fillMaxWidth()) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 16.dp, end = 16.dp)
+                .weight(0.5f),
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
             Text(text = itm.from)
             Text(text = itm.time)
         }
-        Column() {
-            Switch(checked = false, onCheckedChange = {})
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 16.dp, end = 16.dp)
+                .weight(0.5f),
+            horizontalAlignment = Alignment.End,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Switch(
+                checked = false,
+                onCheckedChange = {},
+            )
         }
     }
+    Divider()
 }

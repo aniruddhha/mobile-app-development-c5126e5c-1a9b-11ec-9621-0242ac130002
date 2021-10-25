12c92a584ccebaf7704d68b66396d9b2a9c9ad43
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-25 16:16:01 +0530

    basics of hilt

--- week6/di-intro/app/src/main/java/com/aniruddha/kudalkar/dihiltintro/hiltbasics/MyApp.kt
+++ week6/di-intro/app/src/main/java/com/aniruddha/kudalkar/dihiltintro/hiltbasics/MyApp.kt
@@ -2,6 +2,16 @@ package com.aniruddha.kudalkar.dihiltintro.hiltbasics
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MyApp : Application()
+class MyApp : Application() {
+
+    @Inject lateinit var nwConfig: NwConfig // field injection
+
+    override fun onCreate() {
+        super.onCreate()
+
+        nwConfig.config()
+    }
+}
--- week6/di-intro/app/src/main/java/com/aniruddha/kudalkar/dihiltintro/hiltbasics/NwConfig.kt
+++ week6/di-intro/app/src/main/java/com/aniruddha/kudalkar/dihiltintro/hiltbasics/NwConfig.kt
@@ -1,8 +1,11 @@
 package com.aniruddha.kudalkar.dihiltintro.hiltbasics
 
 import android.util.Log
+import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class NwConfig
 @Inject
 constructor() {

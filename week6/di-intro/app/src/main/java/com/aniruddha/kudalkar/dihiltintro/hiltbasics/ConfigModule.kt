070e55422f094dfa2c12835f098d75a4c95aad59
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-26 13:25:53 +0530

    case study architecture

--- week6/di-intro/app/src/main/java/com/aniruddha/kudalkar/dihiltintro/hiltbasics/ConfigModule.kt
+++ week6/di-intro/app/src/main/java/com/aniruddha/kudalkar/dihiltintro/hiltbasics/ConfigModule.kt
@@ -4,14 +4,14 @@ import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.scopes.ActivityScoped
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module // hey dagger, this will provide/install objects
 @InstallIn(SingletonComponent::class) // in given scope
-object ConfigModule  {
-
-    @Singleton // hey dagger please put this object in application scope
+object ConfigModule {
     @Provides //  hey dagger please create/provide/delete this object
     fun getGson() = Gson()
 }
+
--- week6/di-intro/app/src/main/java/com/aniruddha/kudalkar/dihiltintro/hiltbasics/GenConfig.kt
+++ week6/di-intro/app/src/main/java/com/aniruddha/kudalkar/dihiltintro/hiltbasics/GenConfig.kt
@@ -4,9 +4,8 @@ import android.util.Log
 import javax.inject.Inject
 import javax.inject.Singleton
 
-@Singleton
 class GenConfig
-@Inject // // hey dagger please create/provide/delete this object
+ // // hey dagger please create/provide/delete this object
 constructor() {
     fun genConfig() = Log.i("@ani", "General Config")
 }

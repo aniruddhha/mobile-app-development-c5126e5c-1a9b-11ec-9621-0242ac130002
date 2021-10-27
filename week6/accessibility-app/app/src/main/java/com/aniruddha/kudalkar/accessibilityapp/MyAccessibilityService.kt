1d0ad69c3afec2e4c5b026d52fb4bf9dfeee9f4d
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-27 12:49:12 +0530

    accessibility service : configuration

--- week6/accessibility-app/app/src/main/java/com/aniruddha/kudalkar/accessibilityapp/MyAccessibilityService.kt
+++ week6/accessibility-app/app/src/main/java/com/aniruddha/kudalkar/accessibilityapp/MyAccessibilityService.kt
@@ -1,11 +1,14 @@
 package com.aniruddha.kudalkar.accessibilityapp
 
 import android.accessibilityservice.AccessibilityService
+import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 
 class MyAccessibilityService : AccessibilityService(){
 
-    override fun onAccessibilityEvent(event: AccessibilityEvent?) {  }
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        Log.i("@ani", "Event Captured")
+    }
 
     override fun onInterrupt() { }
 }

71b96f3e86e27c84aa17757d09ae8b4030b85153
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-28 14:17:50 +0530

    room setup - architecture

--- week6/sqlite-room-basics/app/src/main/java/com/aniruddha/kudalkar/sqliteroombasics/db/DealerDatabase.kt
+++ week6/sqlite-room-basics/app/src/main/java/com/aniruddha/kudalkar/sqliteroombasics/db/DealerDatabase.kt
@@ -1,4 +1,13 @@
 package com.aniruddha.kudalkar.sqliteroombasics.db
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
 // class helps you in connecting with db
-class DealerDatabase
+@Database(
+    entities = [Dealer::class],
+    version = 1
+)
+abstract class DealerDatabase : RoomDatabase() {
+    abstract fun dealerDao()
+}

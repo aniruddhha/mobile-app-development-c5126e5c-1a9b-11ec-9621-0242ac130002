bd7f527f3b2efe95d410cfb490568707ca793f46
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-28 10:27:18 +0530

    file handling - read and write

--- week6/file-intro/app/src/main/java/com/aniruddha/kudalkar/filehandlingintro/FirstFragment.kt
+++ week6/file-intro/app/src/main/java/com/aniruddha/kudalkar/filehandlingintro/FirstFragment.kt
@@ -1,5 +1,6 @@
 package com.aniruddha.kudalkar.filehandlingintro
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.aniruddha.kudalkar.filehandlingintro.databinding.FragmentFirstBinding
+import java.lang.StringBuilder
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -35,10 +37,12 @@ class FirstFragment : Fragment() {
 
         binding.btRd.setOnClickListener {
             storageLocationInfo()
+            readInternalStorage()
         }
 
         binding.btWr.setOnClickListener {
-
+            val str = binding.editTextTextPersonName.text.toString()
+            writeInternalStorage(str)
         }
     }
 
@@ -52,11 +56,21 @@ class FirstFragment : Fragment() {
         Log.i("@ani", "External Storage Location - ${requireContext().getExternalFilesDir("")?.absolutePath}")
     }
 
-    private fun writeInternalStorage() {
-
+    private fun writeInternalStorage(str : String) {
+        val fos = requireContext().openFileOutput("my.txt",Context.MODE_PRIVATE)
+        fos.write(str.toByteArray())
+        fos.close()
     }
 
     private fun readInternalStorage() {
-
+        val fis = requireContext().openFileInput("my.txt")
+        val sb = StringBuilder()
+        while(true) {
+            val ch = fis.read()
+            if(ch == -1) break
+            else sb.append(ch)
+        }
+        Log.i("@ani", "File Data As Below")
+        Log.i("@ani", sb.toString())
     }
 }

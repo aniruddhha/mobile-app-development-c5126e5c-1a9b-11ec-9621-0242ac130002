3f11300f608b6abce83a8cecb1698dd437aecaec
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-08 12:48:51 +0530

    testing  basics

--- week8/testing-basics/app/src/main/java/com/aniruddha/kudalkar/testingbasics/util/EmailValidator.kt
+++ week8/testing-basics/app/src/main/java/com/aniruddha/kudalkar/testingbasics/util/EmailValidator.kt
@@ -1,11 +1,17 @@
 package com.aniruddha.kudalkar.testingbasics.util
 
-import android.text.TextUtils
+import java.util.regex.Pattern
 
 class EmailValidator {
     companion object {
-        fun isValid(email: String) =
-            !TextUtils.isEmpty(email) &&
-                    android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+        fun isValid(email: String) = Pattern.compile(
+            "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                    "\\@" +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                    "(" +
+                    "\\." +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                    ")+"
+        ).matcher(email).matches()
     }
 }
--- week8/testing-basics/app/src/test/java/com/aniruddha/kudalkar/testingbasics/EmailTest.kt
+++ week8/testing-basics/app/src/test/java/com/aniruddha/kudalkar/testingbasics/EmailTest.kt
@@ -1,13 +1,33 @@
 package com.aniruddha.kudalkar.testingbasics
 
 import com.aniruddha.kudalkar.testingbasics.util.EmailValidator
-import com.google.common.truth.Truth.assertThat
-import org.junit.Test
+import org.junit.*
+import org.junit.Assert.*
 
 class EmailTest {
 
     @Test
-    fun check_isEmailValid() {
-       assertThat(EmailValidator.isValid("aaa@bbb.com")).isTrue()
+    fun check_isEmailValid_Proper() {
+       assertTrue(EmailValidator.isValid("aaa@bbb.com"))
+    }
+
+    @Test
+    fun check_isEmailValid_No_First_Part() {
+        assertFalse(EmailValidator.isValid("@bbb.com"))
+    }
+
+    @Test
+    fun check_isEmailValid_No_Second_Part() {
+        assertFalse(EmailValidator.isValid("aaa@.com"))
+    }
+
+    @Test
+    fun check_isEmailValid_No_Domain() {
+        assertFalse(EmailValidator.isValid("aaa@bb"))
+    }
+
+    @Test
+    fun check_isEmailValid_Only_Name() {
+        assertFalse(EmailValidator.isValid("aaa@bb"))
     }
 }

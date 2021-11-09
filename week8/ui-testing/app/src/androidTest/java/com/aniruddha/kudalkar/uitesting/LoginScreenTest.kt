ac525b897b7b9252c61f3ee1f94b69930b06c725
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-09 17:44:19 +0530

    Ui testing : advanced Ui testing

--- week8/ui-testing/app/src/androidTest/java/com/aniruddha/kudalkar/uitesting/LoginScreenTest.kt
+++ week8/ui-testing/app/src/androidTest/java/com/aniruddha/kudalkar/uitesting/LoginScreenTest.kt
@@ -3,10 +3,14 @@ package com.aniruddha.kudalkar.uitesting
 import androidx.test.espresso.Espresso.*
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import org.hamcrest.Matchers.*
+import org.hamcrest.core.*
+import org.hamcrest.core.AllOf.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -17,6 +21,7 @@ class LoginScreenTest {
 
     @get:Rule
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
 
     @Test
     fun check_Login() {
@@ -91,5 +96,23 @@ class LoginScreenTest {
         ).perform(
             click()
         )
+
+        /*onData(
+            AllOf.allOf(
+                `is`(instanceOf(Map::class.java)),
+                hasEntry(equalTo("itm"), `is`("abc"))
+            )
+        ).perform(
+            click()
+        )*/
+
+//        onView(
+//            withId(R.id.rec)
+//        ).perform(
+//            RecyclerViewActions.actionOnItemAtPosition<RecVh>(
+//                0,
+//                click()
+//            )
+//        )
     }
 }

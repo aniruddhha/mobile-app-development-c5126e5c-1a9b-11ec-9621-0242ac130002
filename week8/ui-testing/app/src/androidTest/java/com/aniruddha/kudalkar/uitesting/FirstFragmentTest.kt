4273adc0efffa84458c2d052ce9225778b034fe5
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-09 10:39:12 +0530

    Ui testing

---
+++ week8/ui-testing/app/src/androidTest/java/com/aniruddha/kudalkar/uitesting/FirstFragmentTest.kt
@@ -0,0 +1,35 @@
+package com.aniruddha.kudalkar.uitesting
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class FirstFragmentTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun test_Next_Button_Loaded() {
+        onView(withText("NEXT"))
+            .check(
+                matches(isDisplayed())
+            )
+    }
+
+    @Test
+    fun test_EditText_Loaded() {
+        onView(withId(R.id.etDt))
+            .check(
+                matches(isDisplayed())
+            )
+    }
+}

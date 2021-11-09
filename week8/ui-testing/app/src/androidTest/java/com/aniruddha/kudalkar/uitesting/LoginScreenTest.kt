0976155e0e7a73de1669d5a18339ee6e9f08787d
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-09 18:00:05 +0530

    Ui testing : advanced Ui testing

--- week8/ui-testing/app/src/androidTest/java/com/aniruddha/kudalkar/uitesting/LoginScreenTest.kt
+++ week8/ui-testing/app/src/androidTest/java/com/aniruddha/kudalkar/uitesting/LoginScreenTest.kt
@@ -3,7 +3,7 @@ package com.aniruddha.kudalkar.uitesting
 import androidx.test.espresso.Espresso.*
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.*
-import androidx.test.espresso.contrib.RecyclerViewActions
+//import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4

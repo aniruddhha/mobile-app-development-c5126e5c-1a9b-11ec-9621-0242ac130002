66286057dfad02ab0aab45ef3a8da6a7f771a4a2
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-10 12:02:12 +0530

    glide integration

--- week8/third-party-libs/app/src/main/java/com/aniruddha/kudalkar/thirdpartylibs/FirstFragment.kt
+++ week8/third-party-libs/app/src/main/java/com/aniruddha/kudalkar/thirdpartylibs/FirstFragment.kt
@@ -10,6 +10,8 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.navigation.fragment.findNavController
 import com.aniruddha.kudalkar.thirdpartylibs.databinding.FragmentFirstBinding
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -18,6 +20,10 @@ import java.io.InputStream
 import java.net.URL
 import java.net.URLClassLoader
 import java.net.URLConnection
+import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
+
+
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -31,6 +37,8 @@ import java.net.URLConnection
 
 class FirstFragment : Fragment() {
 
+    private val scp  = CoroutineScope(Dispatchers.IO)
+
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
@@ -40,17 +48,14 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        loadImage(binding.imageView)
+        loadWithGlide(binding.imageView)
     }
 
     override fun onDestroyView() {
@@ -58,11 +63,20 @@ class FirstFragment : Fragment() {
         _binding = null
     }
 
+    private fun loadWithGlide(img : ImageView) {
+
+        Glide.with(this)
+            .load("https://cdn.pixabay.com/photo/2017/09/25/13/12/cocker-spaniel-2785074_1280.jpg")
+            .transition(
+                DrawableTransitionOptions.withCrossFade(3500)
+            )
+            .into(img)
+    }
+
     private fun loadImage(img : ImageView) {
 
-        val scp  = CoroutineScope(Dispatchers.IO)
         scp.launch {
-            val url: URL = URL("https://www.freepnglogos.com/uploads/android-logo-png/android-logo-transparent-png-svg-vector-2.png")
+            val url: URL = URL("https://cdn.pixabay.com/photo/2017/09/25/13/12/cocker-spaniel-2785074_1280.jpg")
             val connection: URLConnection = url.openConnection()
             val ips: InputStream = connection.getInputStream()
             val bitmap: Bitmap = BitmapFactory.decodeStream(ips)
